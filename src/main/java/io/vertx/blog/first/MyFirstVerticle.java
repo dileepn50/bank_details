e78ace70d8e854c590fb5622f78d94c64b6316bf
harish <harish.chanti94@gmail.com>
2019-03-19 14:36:07 +0530

    changes

--- src/main/java/io/vertx/blog/first/MyFirstVerticle.java
+++ src/main/java/io/vertx/blog/first/MyFirstVerticle.java
@@ -30,8 +30,7 @@ public class MyFirstVerticle extends AbstractVerticle {
         System.out.println("http.address " + System.getProperty("http.address", "0.0.0.0"));
 
         vertx.createHttpServer().requestHandler(req -> req.response().end("Hello World!"))
-                .listen(
-                        Integer.getInteger("http.port"), System.getProperty("http.address", "0.0.0.0"));
+                .listen(50501, System.getProperty("http.address", "0.0.0.0"));
         //HttpServer server = vertx.createHttpServer(options);
        // System.out.println("server = " + server);
         //server.requestHandler(router::accept).listen(config().getInteger("port"));
